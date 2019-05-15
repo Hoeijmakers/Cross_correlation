1d9c4ea289d030c6ffe0baed993a2cb5a1b60e3c
Jens Hoeijmakers <jens.hoeijmakers@gmail.com>
2019-05-15 12:55:31 +0200

    Finalized clean-CCF, which cleans the CCF of the stellar contribution.

--- lib/cleaning.py
+++ lib/cleaning.py
@@ -3,14 +3,34 @@ def construct_outlier_mask(order,w,c_thresh):
     return(order*0.0+1.0)
 
 
+def clean_ccf(rv,ccf,ccf_e,dp):
+    """This routine normalizes the CCF fluxes and subtracts the average out of
+    transit CCF, using the transit lightcurve as a mask."""
 
-def clean_order(order,w,thresh,mask=None):
     import numpy as np
+    import lib.functions as fun
     import lib.utils as ut
-    ut.typetest('order',order,np.ndarray)
-    ut.typetest('w',w,float)
-    ut.typetest('thresh',thresh,float)
-    shape=np.shape(order)
+    from matplotlib import pyplot as plt
+    import pdb
+    import lib.system_parameters as sp
+    ut.typetest('rv in clean_ccf',rv,np.ndarray)
+    ut.typetest('ccf in clean_ccf',ccf,np.ndarray)
+    ut.typetest('ccf_e in clean_ccf',ccf_e,np.ndarray)
+    ut.typetest('dp in clean_ccf',dp,str)
+    ut.dimtest(ccf,[0,len(rv)])
+    ut.dimtest(ccf_e,[0,len(rv)])
+    ut.nantest('rv in clean_ccf',rv)
+    ut.nantest('ccf in clean_ccf',ccf)
+    ut.nantest('ccf_e in clean_ccf',ccf_e)
 
-    #THIS IS A WORK IN PROGRESS. MAY NOT NEED TO CLEAN THE SPECTRUM BEFORE DOING THE CCF...
-    #ONLY TO CONSTRUCT AN OUTLIER MASK PERHAPS?
+
+    transit=sp.transit(dp)
+
+    meanflux=np.median(ccf,axis=1)#Normalize the baseline flux.
+    meanblock=fun.rebinreform(meanflux,len(rv))
+    ccf_n = ccf/meanblock.T
+    ccf_ne = ccf_e/meanblock.T
+    meanccf=np.mean(ccf_n[transit == 1.0,:],axis=0)
+    meanblock=fun.rebinreform(meanccf,len(meanflux))
+    ccf_nn = ccf_n-meanblock
+    return(ccf_n,ccf_ne,ccf_nn)
