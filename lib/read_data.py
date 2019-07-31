ed1d1f20d38070738823597b44f27ba824aa5ecf
Jens Hoeijmakers <jens.hoeijmakers@gmail.com>
2019-07-31 15:05:37 +0200

    TELLURIC CORRECTION WITH MOLECFIT ADDED!

--- lib/read_data.py
+++ lib/read_data.py
@@ -14,6 +14,7 @@ def read_EXPRES_e2ds(inpath,outname,air=True):
     import sys
     import lib.utils as ut
     import scipy.interpolate
+
     #First check the input:
     ut.typetest('inpath in read_EXPRES_e2ds ',inpath,str)
     ut.typetest('outname in read_EXPRES_e2ds ',outname,str)
@@ -156,7 +157,7 @@ def read_EXPRES_e2ds(inpath,outname,air=True):
 
 
 
-def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
+def read_HARPS_e2ds(inpath,outname,air=True,nowave=False,molecfit=True):
     """THIS IS A PYTHON TRANSLATION OF READ_DATA (BELOW). IT SHOULD NOT WORK
     WITH A PATHS FILE, JUST A FOLDER THAT CONTAINS ONLY FITS FILES AND THEN
     IT WORKS FROM THE KEYWORDS TO DO EVERYTHING AUTOMATICALLY.
@@ -174,6 +175,12 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
     import matplotlib.pyplot as plt
     import sys
     import lib.utils as ut
+    import lib.molecfit as mol
+    import pyfits
+    import copy
+    import lib.operations as ops
+    import scipy.interpolate as interp
+    import pickle
 
     #First check the input:
     ut.typetest('inpath in read_HARPS_e2ds ',inpath,str)
@@ -193,21 +200,26 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
     #The following variables define the lists in which all the necessary data will be stored.
     framename=[]
     header=[]
+    s1dhdr=[]
     type=[]
     texp=np.array([])
     date=[]
     mjd=np.array([])
     ccfmjd=np.array([])
+    s1dmjd=np.array([])
     npx=np.array([])
     nrv=np.array([])
     norders=np.array([])
     e2ds=[]
+    s1d=[]
+    wave1d=[]
     airmass=np.array([])
     berv=np.array([])
     wave=[]
+    blaze=[]
     ccfs=[]
     wavefile_used = []
-    outpath = 'data/'+outname
+    outpath = ut.path('data/'+outname)
     if os.path.exists(outpath) != True:
         os.makedirs(outpath)
 
@@ -216,21 +228,31 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
     sci_count = 0
     wave_count = 0
     ccf_count = 0
+    blaze_count = 0
+    s1d_count = 0
+
     for i in range(N):
         if filelist[i].endswith('e2ds_A.fits'):
             e2ds_count += 1
             print(filelist[i])
-            data,hdr=fits.getdata(inpath+filelist[i],header=True)
-            framename.append(filelist[i])
-            header.append(hdr)
-            type.append(hdr['HIERARCH ESO DPR CATG'])
-            texp=np.append(texp,hdr['EXPTIME'])
-            date.append(hdr['DATE-OBS'])
-            mjd=np.append(mjd,hdr['MJD-OBS'])
-            npx=np.append(npx,hdr['NAXIS1'])
-            norders=np.append(norders,hdr['NAXIS2'])
-            e2ds.append(data)
+            #data,hdr=fits.getdata(inpath+filelist[i],header=True)
+
+            hdul = fits.open(inpath+filelist[i])
+            data = copy.deepcopy(hdul[0].data)
+            hdr = hdul[0].header
+            hdul.close()
+            del hdul[0].data
             if hdr['HIERARCH ESO DPR CATG'] == 'SCIENCE':
+                framename.append(filelist[i])
+                header.append(hdr)
+                type.append(hdr['HIERARCH ESO DPR CATG'])
+                texp=np.append(texp,hdr['EXPTIME'])
+                date.append(hdr['DATE-OBS'])
+                mjd=np.append(mjd,hdr['MJD-OBS'])
+                npx=np.append(npx,hdr['NAXIS1'])
+                norders=np.append(norders,hdr['NAXIS2'])
+                e2ds.append(data)
+
                 sci_count += 1
                 berv=np.append(berv,hdr['HIERARCH ESO DRS BERV'])
                 airmass=np.append(airmass,0.5*(hdr['HIERARCH ESO TEL AIRM START']+hdr['HIERARCH ESO TEL AIRM END']))
@@ -242,10 +264,11 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
 
                     wavedata=read_wave_from_HARPS_header(hdr)
                     wave.append(wavedata)
-            else:
-                berv=np.append(berv,np.nan)
-                airmass=np.append(airmass,np.nan)
+            # else:
+                # berv=np.append(berv,np.nan)
+                # airmass=np.append(airmass,np.nan)
         if filelist[i].endswith('wave_A.fits'):
+            print(filelist[i]+' (wave)')
             if nowave == True:
                 print("WARNING in read_HARPS_e2ds: nowave was set to True but a wave_A file")
                 print("was detected. This wave file is now ignored in favor of the header.")
@@ -254,13 +277,36 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
                 wave.append(wavedata)
                 wave_count += 1
         if filelist[i].endswith('ccf_G2_A.fits'):
-            ccf,hdr=fits.getdata(inpath+filelist[i],header=True)
+            #ccf,hdr=fits.getdata(inpath+filelist[i],header=True)
+            hdul = fits.open(inpath+filelist[i])
+            ccf = copy.deepcopy(hdul[0].data)
+            hdr = hdul[0].header
+            hdul.close()
+            del hdul[0].data
+
             if hdr['HIERARCH ESO DPR CATG'] == 'SCIENCE':
                 #ccftotal+=ccf
                 ccfs.append(ccf)
                 ccfmjd=np.append(ccfmjd,hdr['MJD-OBS'])
                 nrv=np.append(nrv,hdr['NAXIS1'])
                 ccf_count += 1
+
+        if filelist[i].endswith('blaze_A.fits'):
+                print(filelist[i]+' (blaze)')
+                blazedata=fits.getdata(inpath+filelist[i])
+                blaze.append(blazedata)
+                blaze_count += 1
+        if filelist[i].endswith('s1d_A.fits'):
+            hdul = fits.open(inpath+filelist[i])
+            data_1d = copy.deepcopy(hdul[0].data)
+            hdr = hdul[0].header
+            hdul.close()
+            del hdul[0].data
+            if hdr['HIERARCH ESO DPR CATG'] == 'SCIENCE':
+                s1d.append(data_1d)
+                s1dhdr.append(hdr)
+                s1dmjd=np.append(s1dmjd,hdr['MJD-OBS'])
+                s1d_count += 1
     #Now we catch some errors:
     #-The above should have read a certain number of e2ds files.
     #-A certain number of these should be SCIENCE frames.
@@ -269,9 +315,19 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
     #-All orders should have the same number of pixels (this is true for HARPS).
     #-The wave frame should have the same dimensions as the order frames.
     #-If nowave is set, test that all frames used the same wave_A calibrator.
+    #-The blaze file needs to have the same shape as the e2ds files.
+    #-The number of s1d files should be the same as the number of e2ds files.
+
+
     if ccf_count != sci_count:
         print("ERROR in read_HARPS_e2ds: There is a different number of science CCFs as there is science frames.")
         sys.exit()
+    # if e2ds_count != s1d_count:
+    #     print('ERROR in read_HARPS_e2ds: The numbers of 1ds and e2ds files are different.')
+    #     print("These are the files and their types:")
+    #     for i in range(len(type)):
+    #         print('   '+framename[i]+'  %s' % type[i])
+    #     sys.exit()
     if e2ds_count == 0:
         print("ERROR in read_HARPS_e2ds: The input folder (%s) does not contain files ending in e2ds.fits." % inpath)
         sys.exit()
@@ -331,14 +387,51 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
             for i in range(N):
                 print('   '+framename[i]+'  %s' % wavefile_used[i])
             sys.exit()
+    if blaze_count >= 1:
+        blaze=blaze[0]#SELECT ONLY THE FIRST WAVE FRAME. The rest is ignored.
     if np.shape(wave) != np.shape(e2ds[0]):
         print("ERROR in read_HARPS_e2ds: A wave file was detected but its shape (%s,%s) does not match that of the orders (%s,%s)" % (np.shape(wave)[0],np.shape(wave)[1],np.shape(e2ds[0])[0],np.shape(e2ds[0])[1]))
+    if np.shape(blaze) != np.shape(e2ds[0]) and blaze_count > 0:
+        print("ERROR in read_HARPS_e2ds: A blaze file was detected but its shape (%s,%s) does not match that of the orders (%s,%s)" % (np.shape(blaze)[0],np.shape(wave)[1],np.shape(e2ds[0])[0],np.shape(e2ds[0])[1]))
+    if len(s1dhdr) != len(e2ds) and molecfit == True:
+        print('ERROR in read_HARPS_e2ds: The number of s1d SCIENCE files and e2ds SCIENCE files is not the same. (%s vs %s)' % (len(s1dhdr),len(e2ds)))
+        print('Switching off the molecfit option will suppress this error.')
+
+
 
     #Ok, so now we should have ended up with a number of lists that contain all
     #the relevant information of our science frames.
     #We determine how to sort the resulting lists in time:
     sorting = np.argsort(mjd)
     ccfsorting = np.argsort(ccfmjd)
+    s1dsorting = np.argsort(s1dmjd)
+
+
+    #First sort the s1d files for application of molecfit.
+    if molecfit == True:
+        s1dhdr_sorted=[]
+        s1d_sorted=[]
+        for i in range(len(s1dsorting)):
+            s1dhdr_sorted.append(s1dhdr[s1dsorting[i]])
+            s1d_sorted.append(s1d[s1dsorting[i]])
+
+        # print('Molecfit will be executed onto the files in this order:')
+        # for x in s1dhdr_sorted:
+        #     print(x['DATE-OBS'])
+        list_of_wls,list_of_trans = mol.do_molecfit(s1dhdr_sorted,s1d_sorted,load_previous=False)
+
+        mol.write_telluric_transmission_to_file(list_of_wls,list_of_trans,outpath+'telluric_transmission_spectra.pkl')
+
+
+
+
+
+
+
+
+
+
+
     ccftotal = 0.0
     #Now we loop over all exposures and collect the i-th order from each exposure,
     #put these into a new matrix and save them to FITS images:
@@ -350,8 +443,8 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
         wave_axis = wave[i,:]/10.0#Convert to nm.
         print('CONSTRUCTING ORDER %s' % i)
         c = 0#To count the number of science frames that have passed. The counter
-        #c is not equal to j because the list of files contains not only SCIENCE
-        #frames.
+        # c is not equal to j because the list of files contains not only SCIENCE
+        # frames.
         cc = 0#Same for ccfs
         for j in range(len(ccfsorting)):
             ccf=ccfs[ccfsorting[j]]
@@ -360,25 +453,37 @@ def read_HARPS_e2ds(inpath,outname,air=True,nowave=False):
         for j in range(len(sorting)):#Loop over exposures
             if i ==0:
                 print('---'+type[sorting[j]]+'  '+date[sorting[j]])
-            if type[sorting[j]] == 'SCIENCE':
+            if type[sorting[j]] == 'SCIENCE':#This check may be redundant.
                 exposure = e2ds[sorting[j]]
                 order[c,:] = exposure[i,:]
+                #T_i = interp.interp1d(list_of_wls[j],list_of_trans[j])#This should be time-sorted, just as the e2ds files.
+                #Do a manual check here that the MJDs are identical.
+                #Also, determiine what to do with airtovac.
+                #tel_order[c,:] = T_i[wave_axis]
                 #Now I also need to write it to file.
                 if i ==0:#Only do it the first time, not for every order.
                     line = str(mjd[sorting[j]])+'\t'+date[sorting[j]]+'\t'+str(texp[sorting[j]])+'\t'+str(airmass[sorting[j]])+'\t'+str(berv[sorting[j]])+'\t'+framename[sorting[j]]+'\n'
                     f.write(line)
                 c+=1
         ccftotal+=ccforder
-        fits.writeto(outpath+'/ccf_'+str(i)+'.fits',ccforder,overwrite=True)
-        fits.writeto(outpath+'/order_'+str(i)+'.fits',order,overwrite=True)
-        fits.writeto(outpath+'/wave_'+str(i)+'.fits',wave_axis,overwrite=True)
-    fits.writeto(outpath+'/ccftotal.fits',ccftotal,overwrite=True)
+
+
+
+        fits.writeto(outpath+'ccf_'+str(i)+'.fits',ccforder,overwrite=True)
+        fits.writeto(outpath+'order_'+str(i)+'.fits',order,overwrite=True)
+        fits.writeto(outpath+'wave_'+str(i)+'.fits',wave_axis,overwrite=True)
+    fits.writeto(outpath+'ccftotal.fits',ccftotal,overwrite=True)
     f.close()
-    print('obs_times written to '+outpath)
+    print('Time-table written to '+outpath+'obs_times')
     print('WARNING: FORMATTING IS STILL SCREWED UP!')
     print('FIGURE OUT HOW TO FORMAT THOSE LINES IN A MORE HUMAN READABLE WAY')
     print('WHEN YOU HAVE INTERNET AGAIN.')
-    #pdb.set_trace()
+
+
+    #ADD OUTPUT OF THE TELLURIC TRANSMISSION SPECTRUM HERE
+    #Also output the list of trans spectra to the datafolder while checking that the
+    #dates are the same as in the just exported config file.
+
 
 
 def read_wave_from_HARPS_header(h):
